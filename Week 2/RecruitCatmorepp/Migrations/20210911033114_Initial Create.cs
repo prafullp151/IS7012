3658335611684cc1f142c9d9529d759def3c8690
Prafull More <morepp@mail.uc.edu>
2021-09-10 23:51:17 -0400

    working pages

---
+++ Week 2/RecruitCatmorepp/Migrations/20210911033114_Initial Create.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecruitCatmorepp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Industry",
+                columns: table => new
+                {
+                    IndustryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IndustryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Industry", x => x.IndustryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobTitle",
+                columns: table => new
+                {
+                    JobTitleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    MinSalary = table.Column<decimal>(nullable: false),
+                    MaxSalary = table.Column<decimal>(nullable: false),
+                    WorkHrsPerDay = table.Column<string>(nullable: true),
+                    PercentTravelRequired = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobTitle", x => x.JobTitleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(nullable: true),
+                    JobPosition = table.Column<string>(nullable: true),
+                    MinSalary = table.Column<decimal>(nullable: false),
+                    MaxSalary = table.Column<decimal>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    JobLocation = table.Column<string>(nullable: true),
+                    IndustryId = table.Column<int>(nullable: false),
+                    CompanyAddress = table.Column<string>(nullable: true),
+                    AnnualRevenue = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Company_Industry_IndustryId",
+                        column: x => x.IndustryId,
+                        principalTable: "Industry",
+                        principalColumn: "IndustryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                columns: table => new
+                {
+                    CandidateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    TargetSalary = table.Column<decimal>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    CompanyId = table.Column<int>(nullable: true),
+                    JobTitleId = table.Column<int>(nullable: false),
+                    IndustryId = table.Column<int>(nullable: false),
+                    PreferredLocation = table.Column<string>(nullable: true),
+                    OpenForRelocation = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.CandidateId);
+                    table.ForeignKey(
+                        name: "FK_Candidate_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Candidate_Industry_IndustryId",
+                        column: x => x.IndustryId,
+                        principalTable: "Industry",
+                        principalColumn: "IndustryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Candidate_JobTitle_JobTitleId",
+                        column: x => x.JobTitleId,
+                        principalTable: "JobTitle",
+                        principalColumn: "JobTitleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidate_CompanyId",
+                table: "Candidate",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidate_IndustryId",
+                table: "Candidate",
+                column: "IndustryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidate_JobTitleId",
+                table: "Candidate",
+                column: "JobTitleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_IndustryId",
+                table: "Company",
+                column: "IndustryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidate");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "JobTitle");
+
+            migrationBuilder.DropTable(
+                name: "Industry");
+        }
+    }
+}
