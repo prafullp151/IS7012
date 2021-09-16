cfe1390731c1f6c62d0a85dd4b5eb83eea7bd2d8
Prafull More <morepp@mail.uc.edu>
2021-09-16 18:48:43 -0400

    Base model

---
+++ Week 2/Week 3/RecruitCatmorepp/Models/JobTitle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitCatmorepp.Models
+{
+    public class JobTitle
+    {
+        public string Title { get; set; }
+        public decimal MinSalary { get; set; }
+        public decimal MaxSalary { get; set; }
+        public int JobTitleId { get; set; }
+        public List<Candidate> Candidates { get; set; }
+        public string Skills { get; set; }
+        public int PercentTravelRequired { get; set; }
+        public string WorkHrsPerDay { get; set; }
+    }
+
+}
